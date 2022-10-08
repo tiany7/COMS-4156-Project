b9764bc03e4ff41081dac13c302860e365733bd7
tiany7 <yuanhan.tian@vanderbilt.edu>
2022-10-08 18:32:20 +0000

    GRPC Installed

---
+++ services/echo_services/echo_service.cpp
@@ -0,0 +1,41 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+
+#include "proto_messages/echo.h"
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+class echoServiceServiceImpl final : public echoService::Service {
+    Status echo(ServerContext* context, const echoRequest* request,
+               echoResponse* reply) override {
+        reply->set_result(request.message());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    echoServiceServiceImpl service;
+
+    grpc::EnableDefaultHealthCheckService(true);
+    grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+int main(int argc, char** argv) {
+    RunServer();
+    return 0;
+}
