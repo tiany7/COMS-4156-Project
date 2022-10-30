1965ecf5a68bf7c97117147f89274a8896f55c33
cooooooder <coderxiesiqi1996@126.com>
2022-10-30 18:29:24 -0400

    mod test bashfile

--- unit-test.sh
+++ unit-test.sh
@@ -2,7 +2,9 @@
 
 cd ./services/mysql_service/
 bazel test :faculty_service_test
-cd ../am_service/
+cd ./services/am_service/
 bazel test :am_service_test
-cd ../epoch_service/
+cd ./services/epoch_service/
 bazel test :time_service_test
+cd ./unit_test/person_service_test
+bazel test :person_service_test
