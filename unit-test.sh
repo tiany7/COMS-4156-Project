fc0e96b2854d6beae1e608b240b7ba2ff89e4ca2
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-02 19:43:04 -0400

    Update unit-test.sh

--- unit-test.sh
+++ unit-test.sh
@@ -2,7 +2,9 @@
 
 cd ./services/mysql_service/
 bazel test :faculty_service_test
-cd ../am_service/
+cd ../../unit_test/am_service_test/
 bazel test :am_service_test
-cd ../epoch_service/
-bazel test :time_service_test
+cd ../course_service_test/
+bazel test :course_service_test
+cd ../person_service_test/
+bazel test :person_service_test
