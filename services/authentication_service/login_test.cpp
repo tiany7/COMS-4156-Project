5123a1b275cadb2f05f6994191c645a82b083f7d
tiany7 <yt2825@columbia.edu>
2022-12-08 04:54:44 +0000

    here

--- services/authentication_service/login_test.cpp
+++ services/authentication_service/login_test.cpp
@@ -15,12 +15,21 @@ TEST(TestLogin, LogMain) {
   Login login;
   EXPECT_EQ(login.LogIn("admin1", "admin1"), "user not exists");
   login.LogOut("admin");
+  login.LogOut("admin2");
   login.Register("admin", "admin");
+  login.Register("admin2", "admin2");
   auto secret = login.LogIn("admin", "admin");
   bool result = allDigits(secret.c_str());
   login.LogOut("admin");
   EXPECT_EQ(result, true);
   EXPECT_EQ(login.IsLegalOperator("admin", secret), true);
+  EXPECT_EQ(login.IsLegalOperator("admin", "123"), false);
+  EXPECT_EQ(login.IsLegalOperator("admin2", secret), false);
+  EXPECT_EQ(login.IsLegalOperator("admin2", "123"), false);
+  auto secret2 = login.LogIn("admin2", "admin2");
+  EXPECT_EQ(login.IsLegalOperator("admin2", secret2), true);
+  EXPECT_EQ(login.IsLegalOperator("admin2", "123"), false);
+  EXPECT_EQ(login.IsLegalOperator("admin", secret2), false);
 }
 
 int main(int argc, char** argv) {
